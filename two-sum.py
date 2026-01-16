3b0e7a019aeb1634fbdd8017198cf88aaa19771c
flvSantos15 <flvsantos300@gmail.com>
2026-01-16 16:09:11 -0300

    chore: add two sum code

---
+++ two-sum.py
@@ -0,0 +1,6 @@
+def twoSum(nums: list[int], target: int) -> list[int]:
+    hasher = {}
+    for idx, i in enumerate(nums):
+        if hasher.get(i) is not None:
+            return [hasher.get(i), idx]
+        hasher[target-i] = idx
